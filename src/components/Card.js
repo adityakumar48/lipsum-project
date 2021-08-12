b7cadade55742f446679100d93d90deea59597ba
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-08-12 09:31:39 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+          <div className="flex mx-auto flex-nowrap">
+          <div className="mt-5 mx-auto ml-80 container-fluid w-50 grid flex">
+        
+        
+                <h4 className="text-blue-500   font-semibold">01 <span className="text-black ml-2">Why do we use it?</span></h4>
+                <hr className="grid grid-cols-2  bg-gradient-to-r from-blue-400 to-blue-500 h-1 w-60"/>
+                <p className="grid grid-cols-2   mt-2 text-gray-500">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+        
+        
+            </div>
+          <div className="mt-5 mx-auto container-fluid w-50 flex">
+               <div className="row">
+        
+                <h4 className="text-blue-500  font-semibold">02 <span className="text-black ml-2">Why do we use it?</span></h4>
+                <hr className="grid grid-cols-2  bg-gradient-to-r from-blue-400 to-blue-500 h-1 w-60"/>
+                <p className="grid grid-cols-2   mt-2 text-gray-500">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+               </div>
+        
+            </div>
+          </div>
+          <div className="flex mx-auto flex-nowrap">
+          <div className="mt-5 mx-auto ml-80 container-fluid w-50 grid flex">
+        
+        
+                <h4 className="text-blue-500   font-semibold">03 <span className="text-black ml-2">Where does it come from?</span></h4>
+                <hr className="grid grid-cols-2  bg-gradient-to-r from-blue-400 to-blue-500 h-1 w-60"/>
+                <p className="grid grid-cols-2   mt-2 text-gray-500">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, Richard McClintock, a Latin professor at Hampden-Sydney College making it over 2000 years old.</p>
+        
+        
+            </div>
+          <div className="mt-5 mx-auto container-fluid w-50 flex">
+               <div className="row">
+        
+                <h4 className="text-blue-500  font-semibold">04 <span className="text-black ml-2">Where can I get some?</span></h4>
+                <hr className="grid grid-cols-2  bg-gradient-to-r from-blue-400 to-blue-500 h-1 w-60"/>
+                <p className="grid grid-cols-2   mt-2 text-gray-500">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour,ly ature from 45  or randomised words which don't look even sdouif slightly believable.</p>
+               </div>
+        
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Card
---
+++ src/components/MainBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainBar = () => {
+    return (
+        <>
+          <div className="container mx-auto mt-10 px-60 ">
+          <h1 className="text-center text-4xl">Lorem Ipsum</h1>
+            <p className="text-center mt-5  text-gray-500"> Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+</p>
+
+          </div>
+        </>
+    )
+}
+
+export default MainBar
